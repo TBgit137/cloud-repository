24b68eed97c0bc355fb8146fff41071331fd3807
@Larry-Luo-Tb <13310831343@163.com>
2025-04-14 21:04:25 +0800

    Javaweb JDBC basic

--- Code/Java/Javaweb/Prac/JDBC/jdbc-prac/src/com/xiaoyu/jdbc/JDBCstatement.java
+++ Code/Java/Javaweb/Prac/JDBC/jdbc-prac/src/com/xiaoyu/jdbc/JDBCstatement.java
@@ -2,10 +2,9 @@ package com.xiaoyu.jdbc;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.SQLException;
 import java.sql.Statement;
 
-public class JDBCprac {
+public class JDBCstatement {
     public static void main(String[] args) throws Exception {
         // 1. Register the driver
         Class.forName("com.mysql.cj.jdbc.Driver");
@@ -17,28 +16,17 @@ public class JDBCprac {
         Connection conn = DriverManager.getConnection(url, user, password);
 
         // 3. Define the SQL statement
-        String sql = "UPDATE `student gpa` SET gpa = 5.5 WHERE id = 1";
+        String sql = "CREATE DATABASE db1";
 
         // 4. Create a statement
         Statement stmt = conn.createStatement();
 
-        try {
-            // 5. Start the transaction
-            conn.setAutoCommit(false);
+        int rowsAffected = stmt.executeUpdate(sql);
 
-            // 6. Execute the SQL statement
-            int rowsAffected = stmt.executeUpdate(sql); // Rows affected
-
-            // 7. Print the result
-            System.out.println(rowsAffected);
-
-            // 8. Commit the transaction
-            conn.commit();
-        } catch (Exception e) {
-            // 9. Rollback the transaction
-            conn.rollback();
-
-            throw new RuntimeException(e);
+        if (rowsAffected > 0) {
+            System.out.println("Transaction committed successfully.");
+        } else {
+            System.out.println("Transaction failed.");
         }
 
         // 11. Close the resources
